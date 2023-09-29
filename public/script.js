eddc22ce71410618a8d5340e8be90d9628729bd8
August <brickskill3r@gmail.com>
2023-09-29 13:30:04 -0300

    Agregacion de barcos, botones y display de opciones

---
+++ public/script.js
@@ -0,0 +1,18 @@
+const optionContainer= document.querySelector('.option-container')
+const flipButton= document.querySelector('#flip-button')
+
+
+// Option choosing 
+let angle = 0 
+function flip(){
+const optionShips = Array.from(optionContainer.children)
+
+    angle= angle === 0? 90:0
+
+    optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
+}
+    
+//Creating Boards 
+
+
+flipButton.addEventListener('click', flip)
