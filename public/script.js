5f00754d49cc8c83f6dff44f1fefb1ddf6869448
August <brickskill3r@gmail.com>
2023-09-30 15:11:31 -0300

    Agregacion de tableros y grilla

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+
+const gamesBoardContainer= document.querySelector('#gamesboard-container')
 const optionContainer= document.querySelector('.option-container')
 const flipButton= document.querySelector('#flip-button')
 
@@ -6,13 +8,35 @@ const flipButton= document.querySelector('#flip-button')
 let angle = 0 
 function flip(){
 const optionShips = Array.from(optionContainer.children)
-
-    angle= angle === 0? 90:0
-
+    angle = angle === 0 ? 90:0
     optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
 }
     
 //Creating Boards 
+
+const width = 10 
+
+function createBoard(color,user){
+
+        const gameBoardContainer=document.createElement('div')
+        gameBoardContainer.classList.add('game-board')
+        gameBoardContainer.style.backgroundColor = color
+        gameBoardContainer.id = user
+
+        for(let i = 0; i < width * width; i++){
+            const block = document.createElement('div')
+            block.classList.add('block')
+            block.id = i
+            gameBoardContainer.append(block)
+        }
+
+        gamesBoardContainer.append(gameBoardContainer)
+    }
+    createBoard('deepskyblue','player')
+    createBoard('cadetblue','computer')
+
+
+
 
 
 flipButton.addEventListener('click', flip)
